13d7ba30075a0edd33860f371217e1f04f8e8213
qaiscjdw <qaiscjdw@gmail.com>
2021-06-28 01:55:02 +0700

    First commit

--- resources/views/user/viewChekout.blade.php
+++ resources/views/user/viewChekout.blade.php
@@ -71,8 +71,8 @@ $idUser = $usr -> id;
         <header class="bg-custom py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                    <h1 class="display-4 fw-bolder">012345678910</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Silakan transfer sebesar Rp {{number_format($biaya,0,'','.')}} ke rekening diatas</p>
                 </div>
             </div>
         </header>
